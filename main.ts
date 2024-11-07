33c8c7772b695823bd1357c9f9820dea8095193a
davidcotea32 <davidcotea02@gmail.com>
2024-11-07 21:13:28 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,40 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (Math.randomBoolean()))
+})
+input.onButtonPressed(Button.B, function () {
+    led.stopAnimation()
+    basic.showString("Erasing screen,Please Wait!")
+    for (let index = 0; index < 19; index++) {
+        basic.showLeds(`
+            . # # # .
+            # . . . .
+            # . . . .
+            # . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # # # .
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . . #
+            . . . . #
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            . # # # .
+            `)
+    }
+    basic.showString("Screen Erased")
+    basic.clearScreen()
+})
+basic.showString("Lucky or Not?")
